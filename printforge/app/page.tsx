001bfcfa41bd2a14ae7759516aa4e5eed11a26c8
Ayoub Ouaziz <aouaziz@c4r9p7.1337.ma>
2025-05-22 20:26:37 +0100

    Hero

--- printforge/app/page.tsx
+++ printforge/app/page.tsx
@@ -1,13 +1,35 @@
 import Image from "next/image";
+/**
+ * Challenge: Create the About page. This page will be
+ * accessible at the `/about` route. (e.g. printforge.com/about)
+ */
 
 export default function Home() {
   return (
     <main>
-      <p>Your go-to platform for 3D printing files</p>
-      <h1>Discover what’s possible with 3D printing</h1>
-      <p>Join our community of creators and explore a vast library of user-submitted models.</p>
-      <button>Browse models</button>
-      <img src={"/hero-image.png"}></img>
+      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl ">
+        <div className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            Your go-to platform for 3D printing files
+          </p>
+          <h1 className="text-4xl font-bold md:text-5xl ">
+            Discover what’s possible with 3D printing
+          </h1>
+          <p className="text-lg text-gray-600">
+            Join our community of creators and explore a vast library of
+            user-submitted models.
+          </p>
+          <div className="flex gap-4">
+            <button
+              // href="/3d-models"
+              className="px-6 py-3 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
+            >
+              Browse models
+            </button>
+          </div>
+        </div>
+        <img className="rounded-full" src={"/hero-image.png"}></img>
+      </section>
     </main>
   );
 }
