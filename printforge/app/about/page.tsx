5fa6f99d4b5d4b0b510a3afea7ac191a77c75e0e
ayoub ouaziz <ayoubouaziz5@gmail.com>
2025-05-23 01:44:38 +0100

    about page

--- printforge/app/about/page.tsx
+++ printforge/app/about/page.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const page = () => {
   return (
+    <div className='flex flex-col scroll-auto p-5  '>
+      <div className="flex flex-col md:flex-row gap-10">
+      <img src="/hero-image.png"/>
+        
+          <p>ABOUT PRINTFOGE</p>
+          <h1>Empowering makers worldwide</h1>
+          <p>Founded in 2023, PrintForge has quickly become the go-to platform for 3D printing enthusiasts, makers, and professional designers to share and discover amazing STL files for 3D printing.</p>
+          <p>Our mission is to foster a vibrant community where creativity meets technology, enabling anyone to bring their ideas to life through 3D printing.</p>
+        
+      </div>
     <div>
-      
+
+    </div>
     </div>
   )
 }
